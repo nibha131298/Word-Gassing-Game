a28c230691b27efe039a540f3f61de86c42f967a
Nibha Singh <nibhasingh292@gmail.com>
2023-06-07 19:44:26 +0530

    nibha131298

---
+++ word.js
@@ -0,0 +1,49 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programing language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "golang",
+        hint: "programing language"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a small image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find somthing"  
+    }
+    
+]
+
+
